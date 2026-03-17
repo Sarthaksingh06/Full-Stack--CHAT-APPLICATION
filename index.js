555ae97716c1acee567c2b70d96ece949a41eb4c
SARTHAK SINGH <sarthak108094@gmail.com>
2026-03-17 06:44:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-export const THEMES = [
-    "light",
-    "dark",
-    "cupcake",
-    "bumblebee",
-    "emerald",
-    "corporate",
-    "synthwave",
-    "retro",
-    "cyberpunk",
-    "valentine",
-    "halloween",
-    "garden",
-    "forest",
-    "aqua",
-    "lofi",
-    "pastel",
-    "fantasy",
-    "wireframe",
-    "black",
-    "luxury",
-    "dracula",
-    "cmyk",
-    "autumn",
-    "business",
-    "acid",
-    "lemonade",
-    "night",
-    "coffee",
-    "winter",
-    "dim",
-    "nord",
-    "sunset",
-  ];
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import { app, server } from "./lib/socket.js";
+import path from "path";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    
+  })
+}
+app.options("*", cors());
+
+server.listen(PORT, () => {
+  console.log("Server is running on Port: " + PORT);
+  connectDB();
+});
